a01f3194cf60dffeca0cfb317c9da195a12146dd
alihassan1221 <ali.hassan91393@gmail.com>
2025-05-25 01:25:59 +0500

    changes in connection

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require("colors");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://alihassan91393:admin1234@cluster0.x2fwsiz.mongodb.net/fastfooddatabase");
     console.log(`MongoDB Connected ${conn.connection.host}`.bgYellow);
   } catch (error) {
     console.log(`Error : ${error.message}`.bgRed);
---
+++ server.js
@@ -0,0 +1,71 @@
+// const express = require("express");
+// const morgan = require("morgan");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const dotanv = require("dotenv");
+// const { bgCyan } = require("colors");
+// require("colors");
+// const connectDb = require("./config/config");
+// //dotenv config
+// dotanv.config();
+// //db config
+// connectDb();
+// //rest object
+// const app = express();
+
+// //middlwares
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(morgan("dev"));
+
+// //routes
+// app.use("/api/items", require("./routes/itemRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
+// app.use("/api/bills", require("./routes/billsRoute"));
+
+// //port
+// const PORT = process.env.PORT || 8080;
+
+// //listen
+// app.listen(PORT, () => {
+//   console.log(`Server Running On Port ${PORT}`.bgCyan.white);
+// });
+
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { bgCyan } = require("colors");
+const serverless = require("serverless-http");
+require("colors");
+
+const connectDb = require("./config/config");
+
+dotenv.config();
+
+let app;
+
+try {
+  connectDb(); // Make sure this is not crashing
+
+  app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(morgan("dev"));
+
+  // routes
+  app.use("/api/items", require("./routes/itemRoutes"));
+  app.use("/api/users", require("./routes/userRoutes"));
+  app.use("/api/bills", require("./routes/billsRoute"));
+} catch (err) {
+  console.error("App startup failed:", err);
+  throw err; // Let Vercel show the 500 error
+}
+
+module.exports.handler = serverless(app);
